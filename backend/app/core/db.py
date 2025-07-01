ab71c98990457e2af44015aacdb9d18cb62f2864
nemanjam <nemanja.mitic.elfak@hotmail.com>
2025-07-01 08:46:17 +0200

    mode md files into docs folder

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -6,6 +6,7 @@ from app.models import User, UserCreate
 
 engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 
+print(f"SQLALCHEMY_DATABASE_URI: {settings.SQLALCHEMY_DATABASE_URI}")
 
 # make sure all SQLModel models are imported (app.models) before initializing DB
 # otherwise, SQLModel might fail to initialize relationships properly
